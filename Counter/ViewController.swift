bf99ca827a13653eb2d4baf9beb7c3163b7d53d1
Chingiz <dzhabaev@outlook.com>
2023-06-22 02:59:26 +0400

    В Main добавлена кнопка и лейбл со значением счетчика. В ViewController'е установлена связь с лэйблом значения счетчика, а также связь с кнопкой, реализован счетчик.

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var counterValue: UILabel!
+    var counter: Int = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        counterValue.text = "Значение счётчика: \(counter)"
     }
-
-
+    
+    @IBAction func buttonPush(_ sender: Any) {
+        counter += 1
+        counterValue.text = "Значение счётчика: \(counter)"
+    }
 }
 
